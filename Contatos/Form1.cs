53b84337bd84f8675eca97d845a7d49a4d478b5d
ALESF LUIZ <alesfluiz@gmail.com>
2023-06-23 16:38:11 -0300

    Add project files.

---
+++ Contatos/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contatos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Lista de contatos 1.0\nDesenvolvido por Alesf", "About");
+
+        }
+
+        private void pessoasBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.pessoasBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contatosDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'contatosDBDataSet.Pessoas' table. You can move, or remove it, as needed.
+            this.pessoasTableAdapter.Fill(this.contatosDBDataSet.Pessoas);
+
+        }
+    }
+}
---
+++ dbo.Pessoas.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Pessoas]
+(
+	[ContatoId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Nome] NVARCHAR(100) NULL, 
+    [Empresa] NVARCHAR(100) NULL, 
+    [Telefone] NVARCHAR(100) NULL, 
+    [Email] NVARCHAR(100) NULL, 
+    [Cliente] BIT NULL, 
+    [Ultima Ligação] DATETIME NULL
+)
